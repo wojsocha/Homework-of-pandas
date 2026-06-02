b5a07f3a4b0b1fccccc0d8826a51593bb2aba7b1
Wojciech Socha <wojteksocha02@gmail.com>
2026-06-02 23:56:09 +0200

    Solving exercise 7 and creating iris2.xlsx

--- data/Solution_Exercise_7.py
+++ data/Solution_Exercise_7.py
@@ -1,1 +1,6 @@
 import pandas as pd
+
+iris = pd.read_csv("iris.csv")
+with pd.ExcelWriter("iris2.xlsx") as writer:
+    for variety_name, variety_df in iris.groupby("variety"):
+        variety_df.to_excel(writer, sheet_name=variety_name)
